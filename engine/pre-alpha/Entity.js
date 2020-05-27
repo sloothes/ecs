6a7ecf07adfb758358b105c99c1773018b5b2096
sloothes <sloothes.com@gmail.com>
2020-05-27 09:00:56 +0300

    Entity prototype.lenght getter

--- engine/pre-alpha/Entity.js
+++ engine/pre-alpha/Entity.js
@@ -8,31 +8,14 @@
 
 	Entity.prototype = {
 
-		_count: 0,
-		isEntity: true,
-		constructor: Entity,
-
 		get length(){ return Object.keys(this).length - 1 },
-
-		add( key, object ){
-			if ( !key ) throw "EntityAddComponentError: component key is not defined!";
-			this[ key ] = object;
-			return this;
-		},
-
-		remove( key ){
-			if ( !key ) throw "EntityRemoveComponentError: component key is not defined!";
-			delete this[ key ];
-			return this;
-		},
 
 	};
 
-//	Entity.prototype.constructor = Entity;
+	Entity.prototype.constructor = Entity;
+	Entity.prototype._count = 0;
+	Entity.prototype.isEntity = true; // primative type.
 
-//	Entity.prototype._count = 0;
-//	Entity.prototype.isEntity = true; // primative type.
-/*
 	Entity.prototype.add = function(key, object){
 		var err = "EntityAddComponentError: component key is not defined!";
 
@@ -51,4 +34,18 @@
 		delete this[ key ];
 		return this;
 	};
+
+
+/*
+		add( key, object ){
+			if ( !key ) throw "EntityAddComponentError: component key is not defined!";
+			this[ key ] = object;
+			return this;
+		},
+
+		remove( key ){
+			if ( !key ) throw "EntityRemoveComponentError: component key is not defined!";
+			delete this[ key ];
+			return this;
+		},
 */
