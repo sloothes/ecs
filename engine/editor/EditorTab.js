5d164087d87b9d361689fb6149a3981253ef02ba
sloothes <sloothes.com@gmail.com>
2020-06-14 00:57:32 +0300

    Fixed addtoOctree, removefromOctree, updateOctree.

--- engine/editor/EditorTab.js
+++ engine/editor/EditorTab.js
@@ -516,11 +516,18 @@
 		}
 
 		function getObjectByEntityId( value ){
-
 			var check = checkId( value );
 			if ( !check ) return undefined;
 			var id = parseInt( value );
 			return scene.getObjectById( id );
+		}
+
+		function getObjectsByGeometry( uuid ){
+			return scene.children.filter(function(child){
+				return child.geometry && child.geometry.uuid === uuid; // same geometry.
+			}).filter(function( object ){
+				return !localPlayer.getObjectById(object.id); // not localPlayer child.
+			})
 		}
 
 		function resetEntitySelectValue(){
@@ -1334,20 +1341,35 @@
 		})();
 
 		function addtoOctree( value ){
+
+		//	DevNote: You have to add all objects with the 
+		//	same geometry.uuid. 
+		//	READ explanation at removefromOctree comments.
+
 			var object = getObjectByEntityId( value );
 
 			if ( !object ) return;
 			if ( !object.isMesh ) return;
 			if ( !object.geometry ) return;
 			if ( !object.geometry.isGeometry ) return;
-			if ( localPlayer.getObjectById(object.id) ) return; // child of localPlayer.
+			if ( localPlayer.getObjectById(object.id) ) return; // localPlayer child.
 
 		//	Import to octree.
-			octree.importThreeMesh( object );
+		//	octree.importThreeMesh( object );
+		//	Import all objects with same geometry to octree.
+		//	READ explanation at removefromOctree() comments.
+			var uuid = object.geometry.uuid;
+			octree.importThreeMesh.call(octree, getObjectsByGeometry(uuid) );
+
 			return object; // important!
 		}
 
 		function removefromOctree( value ){
+		//	Removes from octree all objects (geometry
+		//	faces) that have the same geometry.uuid.
+		//	DevNote: It would be better if was using
+		//	mesh.uuid and not geometry.uuid. (TODO).
+
 			var object = getObjectByEntityId( value );
 
 			if ( !object ) return;
@@ -1356,8 +1378,11 @@
 			if ( !object.geometry.isGeometry ) return;
 
 		//	Remove from octree.
+		//	DevNote: removes all object's geometry 
+		//	faces that have same geometry.uuid.
 			var uuid = object.geometry.uuid;
 			uuid && octree.removeThreeMesh( uuid );
+
 			return object; // important!
 		}
 
@@ -1368,10 +1393,14 @@
 			if ( !object.isMesh ) return;
 			if ( !object.geometry ) return;
 			if ( !object.geometry.isGeometry ) return;
-			if ( localPlayer.getObjectById(object.id) ) return; // child of localPlayer.
+			if ( localPlayer.getObjectById(object.id) ) return; // localPlayer child.
 
-		//	Import to octree.
-			octree.importThreeMesh( object );
+		//	Import to octree. 
+		//	octree.importThreeMesh( object );
+		//	Use addtoOctree() to import to octree.
+		//	READ explanation at removefromOctree() comments.
+			addtoOctree( value ); 
+
 		}
 
 	//	renamed from "octreeIncluded" to "octreeIncludes".
