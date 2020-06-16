38c7b732773aea2bfddafe89c341b4767b6946d4
sloothes <sloothes.com@gmail.com>
2020-06-16 18:02:33 +0300

    Create MaterialEditor.js

---
+++ engine/editor/MaterialEditor.js
@@ -0,0 +1,55 @@
+
+//	Material Editor.
+
+//	Create a Material to hold editor values.
+	const materialEditor = (function( editor ){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		return editor;
+
+	})( new THREE.Material() );
