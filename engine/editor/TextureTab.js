95c88dcd1cc44f95e45a919cd7f9e029faea9a6a
sloothes <sloothes.com@gmail.com>
2020-06-20 18:18:43 +0300

    Add clone texture button

--- engine/editor/TextureTab.js
+++ engine/editor/TextureTab.js
@@ -361,6 +361,26 @@
 
 	(function( tab ){
 
+	//	Clone texture button.
+	//	var tab = TabUI.Texture.tab;
+
+		var row = document.createElement("h3");
+		row.style.cssText = "display:none;height:40px;margin-bottom:20px;"
+
+		var button = document.createElement("div");
+		button.id = "clone-texture-button";
+		button.textContent = "Clone Texture";
+		button.style.cssText = "width:-webkit-fill-available;float:right;height:40px;font-size:large;margin-right:15px;";
+		button.classList.add( "form-control", "btn", "btn-primary", "btn-white-outline", "gradient-btn" );
+
+		row.appendChild( button );
+		tab.appendChild( row );
+
+	})( TabUI.Texture.tab );
+
+
+	(function( tab ){
+
 	//	Import texture button.
 	//	var tab = TabUI.Texture.tab;
 
