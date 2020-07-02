187b82f2112c02fa93b5a7a9b108d21192a314e3
sloothes <sloothes.com@gmail.com>
2020-07-02 08:26:50 +0300

    move to history

---
+++ engine/editor/history/EntityManagerClass.js
@@ -0,0 +1,165 @@
+//	Entity Manager class.
+
+//	Entity Manager: inherits (extends) Array class.
+//	sources: https://stackoverflow.com/questions/26700164/extending-array-with-es6-classes
+//	https://stackoverflow.com/questions/11337849/ways-to-extend-array-object-in-javascript
+
+	function EntityManager( droplist ){
+		var array = new Array(0);
+		Object.setPrototypeOf( array, EntityManager.prototype );
+		if ( droplist ) array.entity_droplist = droplist;
+		return array; // important!
+	};
+
+	EntityManager.prototype = Object.create(Array.prototype);
+//	EntityManager.prototype.entity_droplist = document.getElementById("entities-droplist"); // manual.
+
+	EntityManager.prototype.move = function( entity, new_index ){
+
+		var old_index = this.findIndex(function( item ){
+			return item.id === entity.id;
+		});
+
+		if ( old_index < 0 ) return; // important!
+		if ( old_index == new_index ) return;
+
+		(function( arr, old_index, new_index ){
+
+			if (new_index >= arr.length) {
+				var k = new_index - arr.length + 1;
+				while (k--) {
+					arr.push(undefined);
+				}
+			}
+
+			arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+
+		})( this, old_index, new_index);
+
+	};
+
+	EntityManager.prototype.add = function(){
+	//	params: {object:Object3D} or {object:entity} 
+
+		if ( arguments.length < 1 ) return;
+
+		var added_objs = [];
+		var adding_ids = [];
+
+	//	Get objects/ids.
+		for ( var i in arguments ) {
+			var param = arguments[i];
+			if ( typeof param === "object" 
+			&& (param.isObject3D || param.isEntity) 
+			&& param.id !== undefined ) {
+				added_objs.push( param );    // keep objects to create options;
+				adding_ids.push( param.id ); // adding_ids.unshift( param.id );
+			} else 
+				continue;
+		}
+
+		if ( !adding_ids.length ) return;
+	//	console.log( "adding_ids:", adding_ids );
+
+		var length = adding_ids.length;
+		for ( var j = 0; j < length; j++ ) {
+			var _id = adding_ids[ j ];
+
+		//	Find index.
+			var index = this.findIndex(function( item ){
+				return item.id === _id;
+			});
+
+			if ( index > -1 ) {
+			//	Remove from added_objs.
+				added_objs.splice(j, 1);
+				continue; // important!
+			}
+
+		//	Add entity.
+			this.push({id:_id});
+		}
+
+	//	global const "entity_droplist" is defined in EditorTab.js;
+	//	var entity_droplist = document.getElementById("entities-droplist");
+
+		if ( !this.entity_droplist ) return; 
+		var entity_droplist = this.entity_droplist;
+
+	//	Add options.
+		while ( added_objs.length ) (function(object){
+			var str =  "", dot = ".", col = ":";
+			var type = "object";
+			var name = object.name;
+			if ( object.type ) type = object.type;
+			var option = document.createElement("option");
+			option.text = str+object.id+dot+type+col+name;
+			option.value = object.id;
+			entity_droplist.appendChild( option );
+		})( added_objs.shift() );
+	};
+
+	EntityManager.prototype.remove = function(){
+	//	params: {number:entity.id} or {object:entity} 
+
+		if ( arguments.length < 1 ) return;
+
+	//	Get removed ids.
+		var remove_ids = [];
+		for ( var i in arguments ) {
+			var param = arguments[i];
+			if ( typeof param === "number" && param % 1 === 0 ) // integer. 
+				remove_ids.unshift( param );    // remove_ids.push( param );
+			else if ( typeof param === "object" 
+			&& (param.isEntity || param.isObject3D) 
+			&& param.id !== undefined )
+				remove_ids.unshift( param.id ); // remove_ids.push( param.id );
+			else 
+				continue;
+		}
+
+		if ( !remove_ids.length ) return;
+	//	console.log( "remove_ids:", remove_ids );
+
+		var length = remove_ids.length;
+		for ( var j = 0; j < length; j++ ) {
+
+			var _id = remove_ids[ j ];
+
+		//	Find index.
+			var index = this.findIndex(function( item ){
+				return item.id === _id;
+			});
+
+			if ( index < 0 ) continue; // important!
+
+			var removedItems = this.splice(index, 1);
+		//	debugMode && console.log( removedItems );
+
+			while ( removedItems.length ){
+				var removed = removedItems.shift();
+			//	debugMode && console.log( removed );
+				removedEntities.push( removed );
+			}
+		}
+
+	//	global const "entity_droplist" is defined in EditorTab.js;
+	//	var entity_droplist = document.getElementById("entities-droplist");
+
+		if ( !this.entity_droplist ) return; 
+		var entity_droplist = this.entity_droplist;
+
+	//	Remove options.
+		while ( remove_ids.length ) (function(id){
+			var selector = "option[value='" + id.toString() + "']"; // console.log( selector );
+			var option = entity_droplist.querySelector( selector ); // console.log(  option  );
+			option && option.remove();
+		})( remove_ids.shift() );
+
+	};
+
+	EntityManager.prototype.clear = function(){
+
+		this.length = 0;
+
+	};
