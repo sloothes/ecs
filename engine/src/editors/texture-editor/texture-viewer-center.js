a2ac0a0657250c2fa8b79e5433ad6a41d70df722
sloothes <sloothes.com@gmail.com>
2020-07-04 22:27:21 +0300

    texture viewer center: on mouse input events.

--- engine/src/editors/texture-editor/texture-viewer-center.js
+++ engine/src/editors/texture-editor/texture-viewer-center.js
@@ -58,6 +58,156 @@
 		getTextureByEntityId // helper function.
 	);
 
+
+//	texture viewer center: on mouse input events.
+
+	(function( editor,viewer,vector_x,vector_y,increase_x,decrease_x,increase_y,decrease_y,vector_droplist,entity_droplist,undo,redo ){
+
+		var state;
+		var interval;
+
+	//	keep first mousedown event, ignore next events.
+
+		increase_x.addEventListener( "mousedown", onfirstMouseDown );
+		decrease_x.addEventListener( "mousedown", onfirstMouseDown );
+		increase_y.addEventListener( "mousedown", onfirstMouseDown );
+		decrease_y.addEventListener( "mousedown", onfirstMouseDown );
+
+		function onfirstMouseDown(){
+			state = {};
+			var key = vector_droplist.value;
+			state.key = vector_droplist.value;
+			state.value = editor[ key ];
+			state.json = editor.toJSON(); // editor json.
+		//	Remove on firstMouseDown event listener.
+			this.removeEventListener( "mousedown", onfirstMouseDown ); // important!
+		//	TODO: You must pass editor undo/redo arrays!
+		//	debugMode && console.log( {"state":state,"undo":undo.length,"redo":redo.length} );
+		};
+
+		increase_x.addEventListener( "mousedown", onMouseDown );
+		decrease_x.addEventListener( "mousedown", onMouseDown );
+		increase_y.addEventListener( "mousedown", onMouseDown );
+		decrease_y.addEventListener( "mousedown", onMouseDown );
+
+		window.addEventListener( "mouseup", function (){
+			clearTimeout( interval ); // important!
+		});
+
+		increase_x.addEventListener( "click", onMouseClick );
+		decrease_x.addEventListener( "click", onMouseClick );
+		increase_y.addEventListener( "click", onMouseClick );
+		decrease_y.addEventListener( "click", onMouseClick );
+
+		function onMouseDown(){ 
+
+			clearTimeout( interval ); // important!
+
+			if ( entity_droplist.value === "" ) return;
+			if ( vector_droplist !== "center" ) return;
+
+			var button = this;
+			var clock = new THREE.Clock();
+
+			interval = setTimeout( function update() {
+
+				var key = vector_droplist.value;
+				if ( key !== "center" ) return;
+
+				var step = 1/100, min = 0, max = 1;
+
+				if ( button === increase_x || button === decrease_x ) {
+					var value = Number(editor.center.x); // get value from editor.
+					if ( button === increase_x ) value = THREE.Math.clamp( value+step, min, max );
+					if ( button === decrease_x ) value = THREE.Math.clamp( value-step, min, max );
+					editor.center.x = round(value, 2); // editor watcher updates vector_x value.
+				}
+
+				else if ( button === increase_y || button === decrease_y ) {
+					var value = Number(editor.center.y); // get value from editor.
+					if ( button === increase_y ) value = THREE.Math.clamp( value+step, min, max );
+					if ( button === decrease_y ) value = THREE.Math.clamp( value-step, min, max );
+					editor.center.y = round(value, 2); // editor watcher updates vector_y value.
+				}
+
+				var dt = clock.getDelta();
+				interval = setTimeout( update, dt );
+			//	debugMode && console.log( "on update:", interval );
+
+			}, 500);
+
+		} // end onMouseDown.
+
+		function onMouseClick(){
+
+			clearTimeout( interval ); // important!
+
+			if ( entity_droplist.value === "" ) return;
+			if ( vector_droplist !== "center" ) return;
+
+			var button = this;
+
+			var key = vector_droplist.value;
+			if ( key !== "center" ) return;
+
+			var step = 1/100, min = 0, max = 1;
+
+		//	Before change the editor[key] value add an undo state in undo queue.
+		//	Until now we has adding to Undo after the value has changed. (FIXED!)
+
+			if ( button === increase_x || button === decrease_x ) {
+				var value = Number(editor.center.x); // get value from editor.
+				if ( button === increase_x ) value = THREE.Math.clamp( value+step, min, max );
+				if ( button === decrease_x ) value = THREE.Math.clamp( value-step, min, max );
+				if ( key === state.key && value !== state.value ) {
+					interval = setTimeout( function(){ 
+					//	Add on firstMouseDown event listener.
+						button.addEventListener( "mousedown", onfirstMouseDown ); // important!
+					//	TODO: You must pass editor undo/redo arrays!
+					//	try { state.json && undo.unshift( state.json ); // Add to undo.
+					//		debugMode && console.log( "undo:", undo.length, "redo:", redo.length ); 
+					//	} catch(err){ console.error("TODO:You must pass undo/redo arrays."); }
+					}, 250);
+				}
+				editor.center.x = round(value, 2); // editor watcher updates vector_x input value.
+			}
+
+			else if ( button === increase_y || button === decrease_y ) {
+				var value = Number(editor.center.y); // get value from editor.
+				if ( button === increase_y ) value = THREE.Math.clamp( value+step, min, max );
+				if ( button === decrease_y ) value = THREE.Math.clamp( value-step, min, max );
+				if ( key === state.key && value !== state.value ) {
+					interval = setTimeout( function(){ 
+					//	Add on firstMouseDown event listener.
+						button.addEventListener( "mousedown", onfirstMouseDown ); // important!
+					//	TODO: You must pass editor undo/redo arrays!
+					//	try { state.json && undo.unshift( state.json ); // Add to undo.
+					//		debugMode && console.log( "undo:", undo.length, "redo:", redo.length ); 
+					//	} catch(err){ console.error("TODO:You must pass undo/redo arrays."); }
+					}, 250);
+				}
+				editor.center.y = round(value, 2); // editor watcher updates vector_y input value.
+			}
+
+		} // end onMouseClick.
+
+	})(
+		textureEditor, // editor,
+		textureViewer, // viewer,
+		document.querySelector("input#texture-vector-x-input"), // vector_x,
+		document.querySelector("input#texture-vector-y-input"), // vector_y,
+		document.querySelector("li#texture-vector-x-increase"), // increase_x,
+		document.querySelector("li#texture-vector-x-decrease"), // decrease_x,
+		document.querySelector("li#texture-vector-y-increase"), // increase_y,
+		document.querySelector("li#texture-vector-y-decrease"), // decrease_y,
+		document.querySelector("select#texture-vector-droplist"), // vector_droplist,
+		document.querySelector("select#texture-entities-droplist") // entity_droplist,
+	//	undo, redo // TODO: pass editor undo/redo arrays.
+	);
+
+
+
+
 //	Experimental (Independent texture viewer center helper).
 //	if not texture entity selected ( !entity_droplist.value )
 //	move texture viewer center but not update texture editor.
