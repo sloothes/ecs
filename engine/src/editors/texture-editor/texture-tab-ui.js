33e74067fd7ec734da1f178bce6d7d28d5d632b4
sloothes <sloothes.com@gmail.com>
2020-07-06 01:23:05 +0300

    Update texture-tab-ui.js

--- engine/src/editors/texture-editor/texture-tab-ui.js
+++ engine/src/editors/texture-editor/texture-tab-ui.js
@@ -382,6 +382,27 @@
 
 	})( TabUI.Texture.tab, getTextureByEntityId );
 
+//	exit-editor-ui.js
+
+	(function( tab ){
+
+	//	Exit edit mode button.
+	//	var tab = TabUI.Texture.tab;
+
+		var row = document.createElement("h3");
+		row.style.cssText = "height:40px;margin-bottom:20px;"
+
+		var button = document.createElement("div");
+		button.id = "texture-exit-mode";
+		button.textContent = "Exit Edit Mode";
+		button.style.cssText = "width:-webkit-fill-available;float:right;height:40px;font-size:large;margin-right:15px;";
+		button.classList.add( "form-control", "btn", "btn-primary", "btn-white-outline", "gradient-btn" );
+
+		row.appendChild( button );
+		tab.appendChild( row );
+
+	})( TabUI.Texture.tab );
+
 //	create-texture-ui.js
 
 	(function( tab ){
@@ -500,27 +521,6 @@
 		button.textContent = "Remove Texture Entity";
 		button.classList.add( "form-control", "btn", "btn-primary", "btn-white-outline", "gradient-btn" );
 		button.style.cssText = "width:-webkit-fill-available;float:right;height:40px;font-size:large;margin-right:15px;";
-
-		row.appendChild( button );
-		tab.appendChild( row );
-
-	})( TabUI.Texture.tab );
-
-//	exit-editor-ui.js
-
-	(function( tab ){
-
-	//	Exit edit mode button.
-	//	var tab = TabUI.Texture.tab;
-
-		var row = document.createElement("h3");
-		row.style.cssText = "height:40px;margin-bottom:20px;"
-
-		var button = document.createElement("div");
-		button.id = "texture-exit-mode";
-		button.textContent = "Exit Edit Mode";
-		button.style.cssText = "width:-webkit-fill-available;float:right;height:40px;font-size:large;margin-right:15px;";
-		button.classList.add( "form-control", "btn", "btn-primary", "btn-white-outline", "gradient-btn" );
 
 		row.appendChild( button );
 		tab.appendChild( row );
