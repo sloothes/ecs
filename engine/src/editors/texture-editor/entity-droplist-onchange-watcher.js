b8a4ab9184db24790a0c8802aa4dc9e31e08a608
sloothes <sloothes.com@gmail.com>
2020-07-06 19:12:14 +0300

    entity-droplist-onchange-watcher.js
    texture_droplist renamed to "map_droplist"

--- engine/src/editors/texture-editor/entity-droplist-onchange-watcher.js
+++ engine/src/editors/texture-editor/entity-droplist-onchange-watcher.js
@@ -1,12 +1,10 @@
+//	entity-droplist-onchange-watcher.js
+
 	(function(editor,key_droplist,vector_droplist,entity_droplist){
 
 	//	blur.
 
-	//	key_droplist.addEventListener( "change", key_droplist.blur );
-	//	vector_droplist.addEventListener( "change", vector_droplist.blur );
-	//	entity_droplist.addEventListener( "change", entity_droplist.blur );
-
-		watch( key_droplist, "onchange", function(){ key_droplist.blur(); }); // EXPERIMANTAL!
+		watch( key_droplist, "onchange", function(){ key_droplist.blur(); });       // EXPERIMANTAL!
 		watch( vector_droplist, "onchange", function(){ vector_droplist.blur(); }); // EXPERIMANTAL!
 		watch( entity_droplist, "onchange", function(){ entity_droplist.blur(); }); // EXPERIMANTAL!
 
@@ -14,9 +12,9 @@
 
 		watch( entity_droplist, "onchange", function( property, event, value ){
 			editor.update( parseInt(value) ); // important! id.
-			var key = key_droplist.value, vector = vector_droplist.value;
-			callWatchers( key_droplist, "onchange", "change", key );
-			callWatchers( vector_droplist, "onchange", "change", vector );
+		//	var key = key_droplist.value, vector = vector_droplist.value;
+			callWatchers( key_droplist, "onchange", "change", key_droplist.value );
+			callWatchers( vector_droplist, "onchange", "change", vector_droplist.value );
 		});
 
 	})(
@@ -25,3 +23,7 @@
 		document.querySelector("select#texture-vector-droplist"), // vector_droplist,
 		document.querySelector("select#texture-entities-droplist") // entity_droplist.
 	);
+
+	//	key_droplist.addEventListener( "change", key_droplist.blur );
+	//	vector_droplist.addEventListener( "change", vector_droplist.blur );
+	//	entity_droplist.addEventListener( "change", entity_droplist.blur );
