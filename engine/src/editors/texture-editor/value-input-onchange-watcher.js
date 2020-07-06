b764c704a67f8f6d2c37fc52c8d58ed43efe9ce8
sloothes <sloothes.com@gmail.com>
2020-07-07 01:45:36 +0300

    fix undo

--- engine/src/editors/texture-editor/value-input-onchange-watcher.js
+++ engine/src/editors/texture-editor/value-input-onchange-watcher.js
@@ -1,6 +1,6 @@
 //	value-input-onchange-watcher.js
 
-	(function(editor,keyInputControls,value_input,key_droplist,entity_droplist){
+	(function(editor,keyInputControls,value_input,key_droplist,entity_droplist,undo_button,redo_button){
 
 		const RAD2DEG = 57.29577951308232;
 		const DEG2RAD = 0.017453292519943295;
@@ -62,35 +62,40 @@
 				case "":
 				case "name":
 				case "uuid":
-					value_input.value = value = ""; // dispbled.
+					value_input.value = value = ""; return; // dispbled.
 				break;
 
 				case "flipY":
-					if ( value === "0" ) value = Number(value);            // accept "0", string.
-					else if ( value === "1" ) value = Number(value);       // accept "1", string.
-					else if ( value.toLowerCase() === "true" ) value = 1;  // accept "true" string.
-					else if ( value.toLowerCase() === "false" ) value = 0; // accept "false" string.
-				//	else if ( isNaN(value) ) value = Boolean(editor[key]); // avoid to pass NaN value!
+					if ( value === "0" ) value = false;                        // accept "0", string.
+					else if ( value === "1" ) value = true;                    // accept "1", string.
+					else if ( value.toLowerCase() === "true" ) value = true;   // accept "true" string.
+					else if ( value.toLowerCase() === "false" ) value = false; // accept "false" string.
+				//	else if ( isNaN(value) ) value = Boolean(editor[key]);     // avoid to pass NaN value!
 					else value = Boolean(editor[key]); // return editor value, important!
 
-					if ( editor[key] !== Boolean(value) ) 
-						try { addtoUndo(); } catch(err) { console.warn("TODO:addtoUndo();"); } // debug!
-				//	editor watcher updates input only if the editor value has changed,
-				//	so in this case we must explicitly update the input value manualy.
-					else value_input.value = editor[key]; // boolean as string.
-				//	editor watcher updates value input.
-					setTimeout( function(){ editor[key] = Boolean(value); });
+					if ( editor[key] !== Boolean(value) ) {
+					//	Before change the editor[key] value add an undo state in undo queue.
+					//	Until now we was adding to undo after the value has changed. (FIXED!)
+						addtoUndo( editor,key,value,undo_button,redo_button ); // add to undo.
+					//	editor watcher updates value input.
+						setTimeout( function(){ editor[key] = Boolean(value); });
+					//	editor watcher updates input only if the editor value has changed,
+					//	so in this case we must explicitly update the input value manualy.
+					} else value_input.value = editor[key]; // boolean as string.
+
 				break;
 
 				case "format":
 					if ( [1021, 1022, 1023, 1024, 1025, 1026, 1027].includes( Number(value) ) ) {
-						if ( editor[key] !== Number(value) ) 
-							try { addtoUndo(); } catch(err) { console.warn("TODO:addtoUndo();"); } // debug!
-					//	editor watcher updates input only if the editor value has changed,
-					//	so in this case we must explicitly update the input value manualy.
-						else value_input.value = editor[key]; // number as string.
-					//	editor watcher updates value input.
-						setTimeout( function(){ editor[key] = Number(value); });
+						if ( editor[key] !== Number(value) ) {
+						//	Before change the editor[key] value add an undo state in undo queue.
+						//	Until now we was adding to undo after the value has changed. (FIXED!)
+							addtoUndo( editor,key,value,undo_button,redo_button ); // add to undo.
+						//	editor watcher updates value input.
+							setTimeout( function(){ editor[key] = Number(value); });
+						//	editor watcher updates input only if the editor value has changed,
+						//	so in this case we must explicitly update the input value manualy.
+						} else value_input.value = editor[key]; // number as string.
 					} 
 					else if ( !isNaN( editor[key]) ) 
 						value_input.value = editor[key];
@@ -99,13 +104,15 @@
 
 				case "mapping":
 					if ( [300, 301, 302, 303, 304, 305, 306, 307].includes( Number(value) ) ) {
-						if ( editor[key] !== Number(value) ) 
-							try { addtoUndo(); } catch(err) { console.warn("TODO:addtoUndo();"); } // debug!
-					//	editor watcher updates input only if the editor value has changed,
-					//	so in this case we must explicitly update the input value manualy.
-						else value_input.value = editor[key]; // number as string.
-					//	editor watcher updates value input.
-						setTimeout( function(){ editor[ key ] = Number(value); });
+						if ( editor[key] !== Number(value) ) {
+						//	Before change the editor[key] value add an undo state in undo queue.
+						//	Until now we was adding to undo after the value has changed. (FIXED!)
+							addtoUndo( editor,key,value,undo_button,redo_button ); // add to undo.
+						//	editor watcher updates value input.
+							setTimeout( function(){ editor[key] = Number(value); });
+						//	editor watcher updates input only if the editor value has changed,
+						//	so in this case we must explicitly update the input value manualy.
+						} else value_input.value = editor[key]; // number as string.
 					} 
 					else if ( !isNaN( editor[key]) ) 
 						value_input.value = editor[key];
@@ -114,13 +121,15 @@
 
 				case "encoding":
 					if ( [3000, 3001, 3007, 3002, 3003, 3004, 3005, 3006, 3200, 3201].includes( Number(value) ) ) {
-						if ( editor[key] !== Number(value) ) 
-							try { addtoUndo(); } catch(err) { console.warn("TODO:addtoUndo();"); } // debug!
-					//	editor watcher updates input only if the editor value has changed,
-					//	so in this case we must explicitly update the input value manualy.
-						else value_input.value = editor[key]; // number as string.
-					//	editor watcher updates value input.
-						setTimeout( function(){ editor[ key ] = Number(value); });
+						if ( editor[key] !== Number(value) ) {
+						//	Before change the editor[key] value add an undo state in undo queue.
+						//	Until now we was adding to undo after the value has changed. (FIXED!)
+							addtoUndo( editor,key,value,undo_button,redo_button ); // add to undo.
+						//	editor watcher updates value input.
+							setTimeout( function(){ editor[key] = Number(value); });
+						//	editor watcher updates input only if the editor value has changed,
+						//	so in this case we must explicitly update the input value manualy.
+						} else value_input.value = editor[key]; // number as string.
 					} 
 					else if ( !isNaN( editor[key]) ) 
 						value_input.value = editor[key];
@@ -129,13 +138,15 @@
 
 				case "magFilter":
 					if ( [1003, 1006].includes( Number(value) ) ) {
-						if ( editor[key] !== Number(value) ) 
-							try { addtoUndo(); } catch(err) { console.warn("addtoUndo();"); } // debug!
-					//	editor watcher updates input only if the editor value has changed,
-					//	so in this case we must explicitly update the input value manualy.
-						else value_input.value = editor[key]; // number as string.
-					//	editor watcher updates value input.
-						setTimeout( function(){ editor[ key ] = Number(value); });
+						if ( editor[key] !== Number(value) ) {
+						//	Before change the editor[key] value add an undo state in undo queue.
+						//	Until now we was adding to undo after the value has changed. (FIXED!)
+							addtoUndo( editor,key,value,undo_button,redo_button ); // add to undo.
+						//	editor watcher updates value input.
+							setTimeout( function(){ editor[key] = Number(value); });
+						//	editor watcher updates input only if the editor value has changed,
+						//	so in this case we must explicitly update the input value manualy.
+						} else value_input.value = editor[key]; // number as string.
 					} 
 					else if ( !isNaN( editor[key]) ) 
 						value_input.value = editor[key];
@@ -144,13 +155,15 @@
 
 				case "minFilter":
 					if ( [1003, 1004, 1005, 1006, 1007, 1008].includes( Number(value) ) ) {
-						if ( editor[key] !== Number(value) ) 
-							try { addtoUndo(); } catch(err) { console.warn("TODO:addtoUndo();"); } // debug!
-					//	editor watcher updates input only if the editor value has changed,
-					//	so in this case we must explicitly update the input value manualy.
-						else value_input.value = editor[key]; // number as string.
-					//	editor watcher updates value input.
-						setTimeout( function(){ editor[ key ] = Number(value); });
+						if ( editor[key] !== Number(value) ) {
+						//	Before change the editor[key] value add an undo state in undo queue.
+						//	Until now we was adding to undo after the value has changed. (FIXED!)
+							addtoUndo( editor,key,value,undo_button,redo_button ); // add to undo.
+						//	editor watcher updates value input.
+							setTimeout( function(){ editor[key] = Number(value); });
+						//	editor watcher updates input only if the editor value has changed,
+						//	so in this case we must explicitly update the input value manualy.
+						} else value_input.value = editor[key]; // number as string.
 					} 
 					else if ( !isNaN( editor[key]) ) 
 						value_input.value = editor[key];
@@ -160,13 +173,15 @@
 				case "wrapS":
 				case "wrapT":
 					if ( [1000, 1001, 1002].includes( Number(value) ) ) {
-						if ( editor[key] !== Number(value) ) 
-							try { addtoUndo(); } catch(err) { console.warn("TODO:addtoUndo();"); } // debug!
-					//	editor watcher updates input only if the editor value has changed,
-					//	so in this case we must explicitly update the input value manualy.
-						else value_input.value = editor[key]; // number as string.
-					//	editor watcher updates value input.
-						setTimeout( function(){ editor[ key ] = Number(value); });
+						if ( editor[key] !== Number(value) ) {
+						//	Before change the editor[key] value add an undo state in undo queue.
+						//	Until now we was adding to undo after the value has changed. (FIXED!)
+							addtoUndo( editor,key,value,undo_button,redo_button ); // add to undo.
+						//	editor watcher updates value input.
+							setTimeout( function(){ editor[key] = Number(value); });
+						//	editor watcher updates input only if the editor value has changed,
+						//	so in this case we must explicitly update the input value manualy.
+						} else value_input.value = editor[key]; // number as string.
 					} 
 					else if ( !isNaN( editor[key]) ) 
 						value_input.value = editor[key];
@@ -175,13 +190,15 @@
 
 				case "anisotropy":
 					if ( !isNaN( Number(value) ) ) {
-						if ( editor[key] !== THREE.Math.clamp(value,-1,1) ) 
-							try { addtoUndo(); } catch(err) { console.warn("TODO:addtoUndo();"); } // debug!
-					//	editor watcher updates input only if the editor value has changed,
-					//	so in this case we must explicitly update the input value manualy.
-						else value_input.value = editor[key].toFixed(2); // number as string.
-					//	editor watcher updates value input.
-						setTimeout(function(){ editor[ key ] = THREE.Math.clamp(value,-1,1); });
+						if ( editor[key] !== THREE.Math.clamp(value,-1,1) ) {
+						//	Before change the editor[key] value add an undo state in undo queue.
+						//	Until now we was adding to undo after the value has changed. (FIXED!)
+							addtoUndo( editor,key,value,undo_button,redo_button ); // add to undo.
+						//	editor watcher updates value input.
+							setTimeout(function(){ editor[ key ] = THREE.Math.clamp(value,-1,1); });
+						//	editor watcher updates input only if the editor value has changed,
+						//	so in this case we must explicitly update the input value manualy.
+						} else value_input.value = editor[key].toFixed(2); // number as string.
 					} 
 					else if ( !isNaN( editor[key]) ) 
 						value_input.value = editor[key].toFixed(2);
@@ -190,13 +207,15 @@
 
 				case "rotation":
 					if ( !isNaN( Number(value) ) ) { 
-						if ( editor[key] !== THREE.Math.clamp(DEG2RAD*value,-Math.PI, Math.PI) ) 
-							try { addtoUndo(); } catch(err) { console.warn("TODO:addtoUndo();"); } // debug!
-					//	editor watcher updates input only if the editor value has changed,
-					//	so in this case we must explicitly update the input value manualy.
-						else value_input.value = (RAD2DEG*editor[key]).toFixed(1); // number as string.
-					//	editor watcher updates value input.
-						setTimeout(function(){ editor[key] = THREE.Math.clamp(DEG2RAD*value,-Math.PI, Math.PI); });
+						if ( editor[key] !== THREE.Math.clamp(DEG2RAD*value,-Math.PI, Math.PI) ) {
+						//	Before change the editor[key] value add an undo state in undo queue.
+						//	Until now we was adding to undo after the value has changed. (FIXED!)
+							addtoUndo( editor,key,value,undo_button,redo_button ); // add to undo.
+						//	editor watcher updates value input.
+							setTimeout(function(){ editor[key] = THREE.Math.clamp(DEG2RAD*value,-Math.PI, Math.PI); });
+						//	editor watcher updates input only if the editor value has changed,
+						//	so in this case we must explicitly update the input value manualy.
+						} else value_input.value = (RAD2DEG*editor[key]).toFixed(1); // number as string.
 					} 
 					else if ( !isNaN( editor[key]) ) 
 						value_input.value = THREE.Math.clamp((RAD2DEG*editor[key]),-180,180).toFixed(1);
@@ -216,7 +235,9 @@
 		keyInputControls, // keyInputControls,
 		document.querySelector("input#texture-value-input"), // value_input,
 		document.querySelector("select#texture-key-droplist"), // key_droplist
-		document.querySelector("select#texture-entities-droplist") // entity_droplist.
+		document.querySelector("select#texture-entities-droplist"), // entity_droplist.
+		document.querySelector("div#texture-undo-button"), // undo_button,
+		document.querySelector("div#texture-redo-button")  // redo_button.
 	);
 
 
