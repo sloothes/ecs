d38c4efaab620b9e07cde161684bda5d4ad676ff
sloothes <sloothes.com@gmail.com>
2020-07-06 15:48:31 +0300

    Update texture-editor.js

--- engine/src/editors/texture-editor/texture-editor.js
+++ engine/src/editors/texture-editor/texture-editor.js
@@ -1,12 +1,15 @@
 //	texture-editor.js
 
+	const textureEditor = new TextureEditor();
+
+/*
 	const textureEditor = (function( entities,viewer ){
 
 		const RAD2DEG = THREE.Math.RAD2DEG;
 		const DEG2RAD = THREE.Math.DEG2RAD;
 
-		const undo = new UndoArray(); debugMode && console.log( {"undo": undo} ); // we could attach undo array on undo_button.
-		const redo = new UndoArray(); debugMode && console.log( {"redo": redo} ); // we could attach redo array on redo_button.
+	//	const undo = new UndoArray(); debugMode && console.log( {"undo": undo} ); // we could attach undo array on undo_button.
+	//	const redo = new UndoArray(); debugMode && console.log( {"redo": redo} ); // we could attach redo array on redo_button.
 
 	//	droplists.
 
@@ -85,49 +88,96 @@
 			return;
 		}
 
-	//	Undo/Redo/Exit buttons.
-
-		(function(undo,redo,exit_button,undo_button,redo_button,entity_droplist){
-
-			var interval;
-
-			undo_button.undo = undo; // we attach undo array on undo_button.undo to parse in functions.
-			redo_button.redo = redo; // we attach redo array on redo_button.redo to parse in functions.
-
-			undo_button.addEventListener( "click", function(){
-				debugMode && console.log("undo:",undo_button.undo.length,"redo:",redo_button.redo.length);
-				if ( entity_droplist.value === "" ) { 
-				//	clearUndoRedo();
-					undo_button.undo.clear();
-					redo_button.redo.clear();
-				}
-				else undo_button.undo.length && editor.undo(); // undo.
-			});
-
-			redo_button.addEventListener( "click", function(){
-				debugMode && console.log("undo:",undo_button.undo.length,"redo:",redo_button.redo.length);
-				if ( entity_droplist.value === "" ) { 
-				//	clearUndoRedo();
-					undo_button.undo.clear();
-					redo_button.redo.clear();
-				}
-				else redo_button.redo.length && editor.redo(); // redo.
-			});
-
-			exit_button.addEventListener( "click", function(){
-				clearTimeout( interval ); 
-				interval = setTimeout( exitFromEditMode, 250);
-			});
-
-		})(
-			undo, redo,
-			document.getElementById("texture-exit-mode"),   // exit_button,
-			document.getElementById("texture-undo-button"), // undo_button,
-			document.getElementById("texture-redo-button"),  // redo_button,
-			document.querySelector("select#texture-entities-droplist") // entity_droplist
-		);
-
 		return editor;
 
 	})( texture_entities, textureViewer );
+*/
+
+//	============================ Undo/Redo/Exit buttons ============================  //
+
+// undo-redo-onmouse-events.js
+
+	(function(editor,undo_button,redo_button,entity_droplist){
+
+		var interval;
+
+		undo_button.undo = undo; // we attach undo array on undo_button.undo to parse in functions.
+		redo_button.redo = redo; // we attach redo array on redo_button.redo to parse in functions.
+
+		undo_button.addEventListener( "click", function(){
+			debugMode && console.log("undo:",undo_button.undo.length,"redo:",redo_button.redo.length);
+			if ( entity_droplist.value === "" ) { 
+			//	clearUndoRedo();
+				undo_button.undo.clear();
+				redo_button.redo.clear();
+			}
+			else undo_button.undo.length && editor.undo(); // undo.
+		});
+
+		redo_button.addEventListener( "click", function(){
+			debugMode && console.log("undo:",undo_button.undo.length,"redo:",redo_button.redo.length);
+			if ( entity_droplist.value === "" ) { 
+			//	clearUndoRedo();
+				undo_button.undo.clear();
+				redo_button.redo.clear();
+			}
+			else redo_button.redo.length && editor.redo(); // redo.
+		});
+
+	})(
+		textureEditor, // editor,
+		document.querySelector("div#texture-undo-button"), // undo_button,
+		document.querySelector("div#texture-redo-button"),  // redo_button,
+		document.querySelector("select#select#texture-entities-droplist") // entity_droplist
+	);
+
+// exit-editor-onmouse-events.js
+
+	(function(editor,viewer,exit_button,undo_button,redo_button,text_input,value_input,vector_x,vector_y,key_droplist,vector_droplist,entity_droplist){
+
+		var interval;
+
+		exit_button.addEventListener( "click", function(){
+			clearTimeout( interval ); 
+			interval = setTimeout( function exitFromEditMode(){
+
+				editor.reset();
+				viewer.reset();
+
+			//	clearUndoRedo();
+				(function() {
+					undo_button.undo.clear();
+					redo_button.redo.clear();
+				})();
+
+			//	resetUIValues();
+				(function() {
+					vector_x.value = "";
+					vector_y.value = "";
+					text_input.value = "";
+					value_input.value = "";
+					key_droplist.value = "";
+					vector_droplist.value = "";
+					entity_droplist.value = "";
+				})();
+
+				return;
+
+			}, 250);
+		});
+
+	})(
+		textureEditor, // editor,
+		textureViewer, // viewer,
+		document.querySelector("div#texture-exit-mode"),   // exit_button,
+		document.querySelector("div#texture-undo-button"), // undo_button,
+		document.querySelector("div#texture-redo-button"),  // redo_button,
+		document.querySelector("input#texture-text-input"), // text_input,
+		document.querySelector("input#texture-value-input"), // value_input,
+		document.querySelector("input#texture-vector-x-input"), // vector_x,
+		document.querySelector("input#texture-vector-y-input"), // vector_y,
+		document.querySelector("select#texture-key-droplist"), // key_droplist,
+		document.querySelector("select#texture-vector-droplist"), // vector_droplist,
+		document.querySelector("select#select#texture-entities-droplist") // entity_droplist
+	);
 
