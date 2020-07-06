0377f40b6745f7964bc7c097614d93bede38be31
sloothes <sloothes.com@gmail.com>
2020-07-06 20:46:34 +0300

    text-input-onchange-watcher.js

---
+++ engine/src/editors/material-editor/text-input-onchange-watcher.js
@@ -0,0 +1,86 @@
+//	text-input-onchange-watcher.js
+
+	(function(editor,keyInputControls,text_input,value_input,key_droplist,entity_droplist,undo_button,redo_button){
+
+	//	blur.
+
+		watch( text_input, "onchange", function(){ text_input.blur(); }); // EXPERIMANTAL!
+
+	//	keyInputControls.
+
+		function enableKeyInputControls(){
+			keyInputControls.isDisabled = false;
+		}
+
+		function disableKeyInputControls(){
+			keyInputControls.isDisabled = true;
+		}
+
+		text_input.addEventListener( "blur", enableKeyInputControls );
+		text_input.addEventListener( "focus", disableKeyInputControls );
+
+	//	add undo.
+
+		function addtoUndo(editor,key,value,undo_button,redo_button){
+			if ( editor[ key ] === value ) return;
+			var json = editor.toJSON();
+			json && undo_button.undo.unshift( json );
+			try { debugMode && console.log( 
+				"undo:", undo_button.undo.length, 
+				"redo:", redo_button.redo.length 
+			); } catch(err){;}
+			return;
+		}
+
+	//	onchange.
+
+		watch( text_input, "onchange", function(property, event, value){
+			debugMode && console.log({item:text_input,event:event,key:key_droplist.value,value:value});
+
+			if ( key_droplist.value === "" ) text_input.value = value = "";
+			if ( entity_droplist.value === "" ) text_input.value = value = "";
+
+			var key = key_droplist.value; // important!
+		//	var value = text_input.value; // value.
+
+			switch ( key ){
+
+				case "uuid":
+				case "type":
+				case "linecap":
+				case "linejoin":
+				case "wireframeLinecap":
+					[ text_input.value, value_input.value ] = [ editor[key], "" ];
+				break;
+
+				case "name":
+
+					if ( value === "" ) { 
+						[text_input.value, value_input.value] = [ editor[key], "" ]; break; // important!
+					}
+
+				//	Before change the editor[key] value add an undo state in undo queue.
+				//	Until now we was adding to undo after the value has changed. (FIXED!)
+					addtoUndo( editor,key,value,undo_button,redo_button ); // add to undo.
+
+					setTimeout( function(){ editor[ key ] = value; });  // important!
+
+				break;
+
+				default:
+					[ text_input.value, value_input.value ] = ["",""];
+				break;
+			}
+
+		});
+
+	})(
+		materialEditor, // editor,
+		keyInputControls, // keyInputControls,
+		document.querySelector("input#material-text-input"), // text_input,
+		document.querySelector("input#material-value-input"), // value_input,
+		document.querySelector("select#material-keys-droplist"), // key_droplist
+		document.querySelector("select#material-entities-droplist"), // entity_droplist.
+		document.querySelector("div#material-undo-button"), // undo_button,
+		document.querySelector("div#material-redo-button") // redo_button (is needed only for the debug console.log).
+	);
--- engine/src/editors/texture-editor/text-input-onchange-watcher.js
+++ engine/src/editors/texture-editor/text-input-onchange-watcher.js
@@ -1,16 +1,9 @@
 //	text-input-onchange-watcher.js
 
-	(function(editor,keyInputControls,text_input,key_droplist,entity_droplist,undo,redo){
-
-		function addtoUndo(){
-			var json = editor.toJSON();
-			json && undo.unshift( json );
-			debugMode && console.log( "undo:", undo.length, "redo:", redo.length ); 
-			return;
-		}
+	(function(editor,keyInputControls,text_input,value_input,key_droplist,entity_droplist,undo_button,redo_button){
 
 	//	blur.
-	//	text_input.addEventListener( "change", text_input.blur );
+
 		watch( text_input, "onchange", function(){ text_input.blur(); }); // EXPERIMANTAL!
 
 	//	keyInputControls.
@@ -26,9 +19,21 @@
 		text_input.addEventListener( "blur", enableKeyInputControls );
 		text_input.addEventListener( "focus", disableKeyInputControls );
 
-	//	onchange.
+	//	add undo.
 
-	//	EXPERIMANTAL.
+		function addtoUndo(editor,key,value,undo_button,redo_button){
+			if ( editor[ key ] === value ) return;
+			var json = editor.toJSON();
+			json && undo_button.undo.unshift( json );
+			try { debugMode && console.log( 
+				"undo:", undo_button.undo.length, 
+				"redo:", redo_button.redo.length 
+			); } catch(err){;}
+			return;
+		}
+
+	//	onchange (EXPERIMANTAL).
+
 		watch( text_input, "onchange", function(property, event, value){
 			debugMode && console.log({item:text_input,event:event,key:key_droplist.value,value:value});
 
@@ -36,29 +41,30 @@
 			if ( entity_droplist.value === "" ) text_input.value = value = "";
 
 			var key = key_droplist.value; // important!
+		//	var value = text_input.value; // value.
 
 			switch ( key ){
 
 				case "uuid":
-					text_input.value = editor[ key ];
+					[text_input.value, value_input.value] = [ editor[key], "" ];
 				break;
 
 				case "name":
 
 					if ( value === "" ) {
-						text_input.value = editor[ key ]; // text_input.value, string.
-						break;
+						[text_input.value, value_input.value] = [ editor[key], "" ]; break;
 					}
 
 				//	Before change the editor[key] value add an undo state in undo queue.
-				//	Until now we has adding to Undo after the value has changed. (FIXED!)
-					if ( editor[ key ] !== value ) addtoUndo(); // add to undo.
+				//	Until now we was adding to undo after the value has changed. (FIXED!)
+					addtoUndo( editor,key,value,undo_button,redo_button ); // add to undo.
+
 					setTimeout(function(){ editor[ key ] = value; }); // text_input.value, string.
 
 				break;
 
 				default:
-					text_input.value = value = "";
+					[text_input.value, value_input.value] = ["",""];
 				break;
 			}
 
@@ -68,10 +74,11 @@
 		textureEditor, // editor,
 		keyInputControls, // keyInputControls,
 		document.querySelector("input#texture-text-input"), // text_input,
+		document.querySelector("input#texture-value-input"), // value_input,
 		document.querySelector("select#texture-key-droplist"), // key_droplist
 		document.querySelector("select#texture-entities-droplist"), // entity_droplist.
-		document.querySelector("div#texture-undo-button").undo, // undo array,
-		document.querySelector("div#texture-redo-button").redo // redo array.
+		document.querySelector("div#texture-undo-button"), // undo_button,
+		document.querySelector("div#texture-redo-button") // redo_button (is needed only for the debug console.log).
 	);
 
 
@@ -91,6 +98,7 @@
 //
 //		});
 
+//		text_input.addEventListener( "change", text_input.blur );
 
 //			var key = key_droplist.value; // important!
 //
