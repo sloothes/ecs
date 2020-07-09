cebc47e3cc3cb6876d5c8af3a64e234cae8b3157
sloothes <sloothes.com@gmail.com>
2020-07-09 07:04:05 +0300

    octree helpers

--- engine/src/editors/geometry-editor/create-geometry-entity.js
+++ engine/src/editors/geometry-editor/create-geometry-entity.js
@@ -2,15 +2,6 @@
 
 	const rigidObjects = []; // cameraControls.rigidObjects;
 
-	function removefromRigidObjects( value ){
-		var index = rigidObjects.findIndex( 
-			function( object ){
-				return object.id === parseInt( value );
-			});
-		if ( index < 0 ) return; // important!
-		rigidObjects.splice( index, 1 );
-	}
-
 	function addtoRigidObjects( value ){
 		var id = parseInt(value);
 	//	if ( !checkId( value ) ) return;
@@ -22,19 +13,13 @@
 		object && object.isMesh && rigidObjects.push( object );
 	}
 
-	function octreeIncludes( uuid ){
-		var result;
-		octree.nodes.forEach(function (nodeDepth) {
-			if ( result ) return;
-			nodeDepth.forEach(function (node) {
-				if ( result ) return;
-				node.trianglePool.forEach(function (face) {
-					if ( result ) return;
-					if (face.meshID === uuid) result = true;
-				});
+	function removefromRigidObjects( value ){
+		var index = rigidObjects.findIndex( 
+			function( object ){
+				return object.id === parseInt( value );
 			});
-		});
-		return result;
+		if ( index < 0 ) return; // important!
+		rigidObjects.splice( index, 1 );
 	}
 
 	function enableCameraRigidObjects(){
@@ -49,6 +34,91 @@
 			var object = cameraControls.rigidObjects.shift()
 			object.isMesh && rigidObjects.push( object ); // cleanup.
 		}
+	}
+
+
+//	octree-helpers.js
+
+	function addtoOctree( value ){
+
+	//	DevNote: You have to add all objects with the 
+	//	same geometry.uuid. 
+	//	READ explanation at removefromOctree comments.
+
+		var object = getObjectByEntityId( value );
+
+		if ( !object ) return;
+		if ( !object.isMesh ) return;
+		if ( !object.geometry ) return;
+		if ( !object.geometry.isGeometry ) return;
+		if ( localPlayer.getObjectById(object.id) ) return; // localPlayer child.
+
+	//	Import to octree.
+	//	octree.importThreeMesh( object );
+	//	Import all objects with same geometry to octree.
+	//	READ explanation at removefromOctree() comments.
+		var uuid = object.geometry.uuid;
+		var meshes = getObjectsByGeometry(uuid);
+		while ( meshes.length ) {
+			octree.importThreeMesh( meshes.shift() );
+		}
+
+		return object; // important!
+	}
+
+	function removefromOctree( value ){
+	//	Removes from octree all objects (geometry
+	//	faces) that have the same geometry.uuid.
+	//	DevNote: It would be better if was using
+	//	mesh.uuid and not geometry.uuid. (TODO).
+
+		var object = getObjectByEntityId( value );
+
+		if ( !object ) return;
+		if ( !object.isMesh ) return;
+		if ( !object.geometry ) return;
+		if ( !object.geometry.isGeometry ) return;
+
+	//	Remove from octree.
+	//	DevNote: removes all object's geometry 
+	//	faces that have same geometry.uuid.
+		var uuid = object.geometry.uuid;
+		uuid && octree.removeThreeMesh( uuid );
+
+		return object; // important!
+	}
+
+	function updateOctree( value ){
+		var object = removefromOctree( value );
+
+		if ( !object ) return;
+		if ( !object.isMesh ) return;
+		if ( !object.geometry ) return;
+		if ( !object.geometry.isGeometry ) return;
+		if ( localPlayer.getObjectById(object.id) ) return; // localPlayer child.
+
+	//	Import to octree. 
+	//	octree.importThreeMesh( object );
+	//	Use addtoOctree() to import to octree.
+	//	READ explanation at removefromOctree() comments.
+		addtoOctree( value ); 
+
+	}
+
+//	renamed from "octreeIncluded" to "octreeIncludes".
+	function octreeIncludes( uuid ){
+		var result;
+		octree.nodes.forEach(function (nodeDepth) {
+			if ( result ) return;
+			nodeDepth.forEach(function (node) {
+				if ( result ) return;
+				node.trianglePool.forEach(function (face) {
+					if ( result ) return;
+					if (face.meshID === uuid) result = true;
+				});
+			});
+		});
+		return result;
 	}
 
 
