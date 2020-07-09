b8730857525e277ffe0c4c516e079e8b36b26256
sloothes <sloothes.com@gmail.com>
2020-07-09 19:40:43 +0300

    Update vector-droplist-onchange-watchers.js

--- engine/src/editors/geometry-editor/vector-droplist-onchange-watchers.js
+++ engine/src/editors/geometry-editor/vector-droplist-onchange-watchers.js
@@ -8,33 +8,37 @@
 		watch(vector_droplist, "onchange", function( property, event, key ){
 			debugMode && console.log({item:vector_droplist,event:event,key:key});
 
-			if ( editor[key] === undefined ) return [vector_x.value, vector_y.value, vector_z, vector_w] = ["","","",""];
+			if ( editor[key] === undefined ) {
+				[vector_x.value, vector_y.value, vector_z.value, vector_w.value] = ["","","",""]; return;
+			}
 
 			switch ( key ) {
 				case "":
-					 [vector_x.value, vector_y.value, vector_z, vector_w] = ["","","",""];
+					[vector_x.value, vector_y.value, vector_z.value, vector_w.value] = ["","","",""];
 				break;
 				case "position":
-					[vector_x.value, vector_y.value, vector_z, vector_w] = [
+					[vector_x.value, vector_y.value, 
+					vector_z.value, vector_w.value] = [
 						editor[key].x.toFixed(2), 
 						editor[key].y.toFixed(2), 
 						editor[key].z.toFixed(2), "" ];
 				break;
 				case "rotation":
-					[vector_x.value, vector_y.value, vector_z, vector_w] = [
+					[vector_x.value, vector_y.value, 
+					vector_z.value, vector_w.value] = [
 						(RAD2DEG*editor[key]._x).toFixed(1), 
 						(RAD2DEG*editor[key]._y).toFixed(1), 
 						(RAD2DEG*editor[key]._z).toFixed(1), "" ];
 				break;
 				case "scale":
-					[vector_x.value, vector_y.value, vector_z, vector_w] = [
+					[vector_x.value, vector_y.value, vector_z.value, vector_w.value] = [
 						(100*editor[key].x).toFixed(1), 
 						(100*editor[key].y).toFixed(1), (100*editor[key].z).toFixed(1), 
 						(100*(editor[key].x+editor[key].y+editor[key].z)/3).toFixed(1) 
 					];
 				break;
 				case "quaternion":
-					[vector_x.value, vector_y.value, vector_z, vector_w] = [
+					[vector_x.value, vector_y.value, vector_z.value, vector_w.value] = [
 						editor[key]._x.toFixed(3), editor[key]._y.toFixed(3), 
 						editor[key]._z.toFixed(3), editor[key]._w.toFixed(3) 
 					];
