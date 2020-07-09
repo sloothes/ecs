9994b283f473bd3d809fc87a05d20922243f4336
sloothes <sloothes.com@gmail.com>
2020-07-09 21:01:57 +0300

    Create vector-input-onchange-watchers.js

---
+++ engine/src/editors/geometry-editor/vector-input-onchange-watchers.js
@@ -0,0 +1,52 @@
+//	vector-input-onchange-watchers.js
+
+//	vector-x input.
+
+	(function(editor,vector_x,vector_droplist,entity_droplist){
+
+		const RAD2DEG = 57.29577951308232;
+		const DEG2RAD = 0.017453292519943295;
+
+		watch( vector_x, "onchange", function(property, event, value){
+			debugMode && console.log({item:vector_x,event:event,key:vector_droplist.value,x:value});
+
+			var key = vector_droplist.value; // important!
+		//	value is vector_x.value, always as typeof "string", (ecxept NaN, undefined?)
+
+		//	We dont "escape", will use editor values on create geometry.
+		//	if ( entity_droplist.value === "" ) return vector_x.value = "";
+		//	if ( vector_droplist.value === "" ) return vector_x.value = "";
+
+			switch (key) {
+				case "scale":
+					if ( isNaN(value) ) value = 1; // avaid NaN value, reset to 100%.
+					else if ( !Number(value) ) value = 1; // avoid scale:0, reset.
+					else value = Number(value)/100; // internal scale value 1/100.
+				break;
+				case "rotation":
+					if ( isNaN(value) ) value = 0; // avaid NaN value, reset!
+					else value = DEG2RAD * THREE.Math.clamp(Number(value), -180, 180); // rad, important!
+				break;
+				case "position":
+					if ( isNaN(value) ) value = 0; // avaid NaN value, reset!
+					else value = Number(value); // meters, no limit.
+				break;
+				case "quaternion":
+					value = 0; // or editor[key]?, no modification.
+				break;
+				default:
+					vector_x.value = ""; return; // escape!
+				break;
+			}
+
+		//	editor manager watcher updates input values.
+			setTimeout(function(value){ editor[key].x = Number(value); }, null, value);
+
+		});
+
+	})(
+		sceneEditor, // editor,
+		document.querySelector("input#geometry-vector-x-input"), // vector_x,
+		document.querySelector("select#geometry-vector-droplist"), // vector_droplist.
+		document.querySelector("select#geometry-entities-droplist") // entity_droplist, not used!!!
+	);
