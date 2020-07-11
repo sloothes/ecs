e197d61ee5b52d5f55f105a056fdd1fbe17756c6
sloothes <sloothes.com@gmail.com>
2020-07-11 19:29:08 +0300

    Update vector-input-onmouse-events.js

--- engine/src/editors/geometry-editor/vector-input-onmouse-events.js
+++ engine/src/editors/geometry-editor/vector-input-onmouse-events.js
@@ -303,3 +303,155 @@
 		document.querySelector("div#geometry-undo-button"), // undo_button,
 		document.querySelector("div#geometry-redo-button")  // redo_button, (only needed for the debug console.log).
 	);
+
+
+//	vector-z mouse input.
+
+	(function( editor,vector_z,increase_z,decrease_z,vector_droplist,entity_droplist,undo_button,redo_button ){
+
+		var state;
+		var interval;
+
+	//	keep first mousedown event, ignore next events.
+
+		increase_z.addEventListener( "mousedown", onfirstMouseDown );
+		decrease_z.addEventListener( "mousedown", onfirstMouseDown );
+
+		var meta = {geometries:{},materials:{},textures:{},images:{},shapes:{}};
+
+		function onfirstMouseDown(){
+			state = {};
+			if ( entity_droplist.value === "" ) return; // important!
+			if ( vector_droplist.value === "quaternion" ) return; // important!
+			var key = vector_droplist.value;
+			state.key = vector_droplist.value; // key.
+			state.value = editor[key].z;   // important!
+			state.json = editor.toJSON( meta ); // editor json.
+			debugMode && console.log( {state:state,meta:meta} ); // debug!
+		//	Remove on firstMouseDown event listener.
+			this.removeEventListener( "mousedown", onfirstMouseDown ); // important!
+		};
+
+		increase_z.addEventListener( "mousedown", onMouseDown );
+		decrease_z.addEventListener( "mousedown", onMouseDown );
+		window.addEventListener( "mouseup", function (){
+			clearTimeout( interval ); // important!
+		});
+		increase_z.addEventListener( "click", onMouseClick );
+		decrease_z.addEventListener( "click", onMouseClick );
+
+		function onMouseDown(){ 
+
+			clearTimeout( interval ); // important!
+
+		//	We dont "escape", will use editor values on create geometry.
+		//	if ( entity_droplist.value === "" ) return; // important!
+		//	if ( vector_droplist.value === "quaternion" ) return;
+
+			var button = this;
+			var clock = new THREE.Clock();
+
+			interval = setTimeout( function update() {
+
+				var key = vector_droplist.value;
+			//	if ( key === "quaternion" ) return;
+
+				switch ( key ) {
+					case "position":
+						var p = 2, step = 1/Math.pow(10,p); // min = -100, max = 100;
+						var value = Number(editor[ key ].z); // get value from editor.
+						if ( button === increase_z ) value += step;
+						if ( button === decrease_z ) value -= step;
+						editor[ key ].z = round(value, p); // editor watcher updates input value.
+						interval = setTimeout( update, clock.getDelta() );
+					break;
+					case "rotation":
+						var p = 1, step = (1/Math.pow(10,p)) * Math.PI/180; // 0.1 deg.
+						var max = Math.PI, min = -max;
+						var value = Number(editor[ key ].z); // get value from editor.
+						if ( button === increase_z ) value = THREE.Math.clamp( value+step, min, max );
+						if ( button === decrease_z ) value = THREE.Math.clamp( value-step, min, max );
+						editor[ key ]._z = round(value, p); // editor watcher updates input value.
+						interval = setTimeout( update, clock.getDelta() );
+					break;
+					case "scale":
+						var p = 3, step = 1/Math.pow(10,p); // min = -100, max = 100;
+						var value = Number(editor[ key ].z); // get value from editor.
+						if ( button === increase_z ) value += step;
+						if ( button === decrease_z ) value -= step;
+						editor[ key ].z = round(value, p); // editor watcher updates input value.
+						interval = setTimeout( update, clock.getDelta() );
+					break;
+				}
+
+			}, 500);
+
+		} // end onMouseDown.
+
+	//	TODO: add undo.
+	//	function addtoUndo(state,key,value,undo_button,redo_button){
+	//		if ( state.key !== key ) return;
+	//		if ( state.value === value ) return;
+	//		state.json && undo_button.undo.unshift( state.json );
+	//		try { debugMode && console.log( 
+	//			"undo:", undo_button.undo.length, 
+	//		"redo:", redo_button.redo.length 
+	//		); } catch(err){;}
+	//		return;
+	//	}
+
+		function onMouseClick(){
+
+			clearTimeout( interval ); // important!
+
+		//	We dont "escape", will use editor values on create geometry.
+		//	if ( entity_droplist.value === "" ) return; // important!
+		//	if ( vector_droplist.value === "quaternion" ) return;
+
+			var button = this;
+
+			var key = vector_droplist.value;
+		//	if ( key === "quaternion" ) return;
+
+			switch ( key ) {
+				case "position":
+					var p = 2, step = 1/Math.pow(10,p); // min = -100, max = 100;
+					var value = Number(editor[ key ].z); // get value from editor.
+					if ( button === increase_z ) value += step;
+					if ( button === decrease_z ) value -= step;
+					editor[ key ].z = round(value, p); // editor watcher updates input value.
+					interval = setTimeout( update, clock.getDelta() );
+				break;
+				case "rotation":
+					var p = 1, step = (1/Math.pow(10,p)) * Math.PI/180; // 0.1 deg.
+					var max = Math.PI, min = -max;
+					var value = Number(editor[ key ].z); // get value from editor.
+					if ( button === increase_z ) value = THREE.Math.clamp( value+step, min, max );
+					if ( button === decrease_z ) value = THREE.Math.clamp( value-step, min, max );
+					editor[ key ]._z = round(value, p); // editor watcher updates input value.
+					interval = setTimeout( update, clock.getDelta() );
+				break;
+				case "scale":
+					var p = 3, step = 1/Math.pow(10,p); // min = -100, max = 100;
+					var value = Number(editor[ key ].z); // get value from editor.
+					if ( button === increase_z ) value += step;
+					if ( button === decrease_z ) value -= step;
+					editor[ key ].z = round(value, p); // editor watcher updates input value.
+					interval = setTimeout( update, clock.getDelta() );
+				break;
+			}
+
+			debugMode && console.log( "on Mouse Click:", interval );
+
+		} // end onMouseClick.
+
+	})(
+		objectEditor, // editor,
+		document.querySelector("input#geometry-vector-z-input"), // vector_z,
+		document.querySelector("li#geometry-vector-z-increase"), // increase_z,
+		document.querySelector("li#geometry-vector-z-decrease"), // decrease_z,
+		document.querySelector("select#geometry-vector-droplist"), // vector_droplist,
+		document.querySelector("select#geometry-entities-droplist"), // entity_droplist,
+		document.querySelector("div#geometry-undo-button"), // undo_button,
+		document.querySelector("div#geometry-redo-button")  // redo_button, (only needed for the debug console.log).
+	);
